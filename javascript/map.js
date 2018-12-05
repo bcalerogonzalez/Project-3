958301813ca75df297b26ae4fa1234883d94ff70
bcalerogonzalez <42592579+bcalerogonzalez@users.noreply.github.com>
2018-12-05 15:55:39 -0600

    Update map.js
    fixing marker

--- javascript/map.js
+++ javascript/map.js
@@ -1,34 +1,14 @@
 function initMap() {
   var map
   var crash = {lat: 18.458954, lng: -67.163973};
-  var villa = {lat: 18.512994, lng: -67.102764};
-  var lajas = {lat: 17.975064, lng: -67.049486};
 		
  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: crash
   });
 	
-  var marker1 = new google.maps.Marker({
-    position: (puerto),
-    map: map
-  });
-}
-
- var marker2 = new google.maps.Marker({
+  var marker = new google.maps.Marker({
     position: (crash),
-    map: map
-  });
-}
-
- var marker3 = new google.maps.Marker({
-    position: (villa),
-    map: map
-  });
-}
-
- var marker4 = new google.maps.Marker({
-    position: (lajas),
     map: map
   });
 }
