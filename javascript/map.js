12d57902694301c4b505ed4a368226e4667f8175
bcalerogonzalez <42592579+bcalerogonzalez@users.noreply.github.com>
2018-12-05 14:58:11 -0600

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,38 +1,32 @@
 function initMap(){
-	  var map;
-      var puerto = {lat: 18.158912, lng: -66.506909};
-      var crash = {lat: 18.458954, lng: -67.163973};
-      var villa = {lat: 18.512994, lng: -67.102764};
-      var lajas = {lat: 17.975064, lng: -67.049486};
+  var puerto = {lat: 18.158912, lng: -66.506909};
+  var crash = {lat: 18.458954, lng: -67.163973};
+  var villa = {lat: 18.512994, lng: -67.102764};
+  var lajas = {lat: 17.975064, lng: -67.049486};
 	
-	  map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: puerto
-        });
+  var map=new google.maps.Map(
+    document.getElementById('map'), {
+    zoom: 4, center: puerto
+    }
+  ); 
 	
-	  var marker1=new google.maps.Marker({
-	    position: puerto,
-	    map: map
-	  });
+  var marker1=new google.maps.Marker({
+    position: puerto,
+    map: map
+  });
 	
-	  var marker2=new google.maps.Marker({
-	    position: crash,
-	    map: map
-	  });
+  var marker2=new google.maps.Marker({
+    position: crash,
+    map: map
+  });
 	
-	  var marker3=new google.maps.Marker({
-	    position: villa
-	    map: map
-	  });
+  var marker3=new google.maps.Marker({
+    position: villa
+    map: map
+  });
 		
-		 var marker3=new google.maps.Marker({
-	    position: lajas
-	    map: map
-	  });
-		
-		function goto(location){
-       map.panTo(location);
-       map.setZoom(15);
-       }
-			
+ var marker3=new google.maps.Marker({
+    position: lajas
+    map: map
+  });		
 }
