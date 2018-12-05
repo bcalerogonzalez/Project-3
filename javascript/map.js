3616c7bdb430d96ee3567171fa320c2009c7d2a8
bcalerogonzalez <42592579+bcalerogonzalez@users.noreply.github.com>
2018-12-05 15:58:35 -0600

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,19 +1,19 @@
 function initMap() {
-  var map
+  var map;
+  var puerto = {lat: 18.158912, lng: -66.506909};
   var crash = {lat: 18.458954, lng: -67.163973};
-		
- var map = new google.maps.Map(document.getElementById('map'), {
+  var villa = {lat: 18.512994, lng: -67.102764};
+  var lajas = {lat: 17.975064, lng: -67.049486};
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: crash
+    center: puerto
   });
-	
   var marker = new google.maps.Marker({
-    position: (crash),
+    position: (puerto, crash, villa, lajas),
     map: map
   });
 }
-
 function goto(location){
   map.panTo(location);
-  map.setzoom(15);
+  map.setZoom(15);
 }
